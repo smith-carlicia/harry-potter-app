9a14e21f635accd28ca97993540f74944f0625ec
carly smith <smith.carlicia@gmail.com>
2024-02-26 18:25:26 -0500

    added buttons and icons to home page

---
+++ app/components/staffBtnCard/staffBtnCard.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import { GiCastle } from "react-icons/gi";
+
+const StaffBtnCard = ({ proUrl, info }) => {
+    return (
+        <div class="relative flex items-center justify-center h-auto w-full  rounded-sm group mb-4" alt={info}>
+            <div>
+                <GiCastle class="flex items-center justify-center text-[#365F5E] text-8xl hover:animate-pulse pb-15"/>
+                <button type="button" class="px-10 py-3  mr-10 mt-8 rounded-md bg-[#231C33] hover:bg-[#3A3366] flex items-center justify-center shadow-lg shadow-slate-800">
+                    <Link class='no-underline  font-[europa] text-white font-semibold text-lg tracking-wider' href={proUrl}>
+                        Staff
+                    </Link>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default StaffBtnCard;
--- app/page.js
+++ app/page.js
@@ -1,11 +1,32 @@
-import Image from "next/image";
+import StaffBtnCard from "./components/staffBtnCard/staffBtnCard";
+import StudentBtnCard from "./components/studentBtnCard/studentBtnCard";
+import SpellsBtnCard from "./components/spellsBtnCard/spellsBtnCard";
+// import { GiSpellBook } from "react-icons/gi";
+
+
+
+
 
 export default function Home() {
   return (
     <div id='home' className='twinkle w-full h-screen  bg-gradient-to-b from-[#222136] from-10% via-[#272A44] via-30% to-[#2C2D4A] to-90% ... '>
       <div className='max-w-[1240px] w-full h-full mx-auto flex justify-center items-center text-center'>
         <div class="row">
-          <h2 class="text-white">WELCOME TO THE WORLD OF HARRY POTTER</h2>
+          <h2 class="text-white p-10">WELCOME TO THE WORLD OF HARRY POTTER</h2>
+          <div class='flex flex-row space-x-20 space-y-19'>
+            <StaffBtnCard 
+              proUrl={'./pages/staff/page'} 
+              alt="staffbutton" 
+            />
+            <StudentBtnCard 
+              proUrl={'./pages/student/page'} 
+              alt="studentsbutton" 
+            />
+            <SpellsBtnCard 
+              proUrl={'./pages/spells/page'} 
+              alt="studentsbutton"
+            />
+          </div>
         </div>
       </div>
     </div>
