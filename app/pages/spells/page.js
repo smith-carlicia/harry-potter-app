b3323e1d60bf05a691eeec6fb10471ce42f9a586
carly smith <smith.carlicia@gmail.com>
2024-02-27 13:57:04 -0500

    fetched spells api and added mapped some data

--- app/pages/spells/page.js
+++ app/pages/spells/page.js
@@ -1,13 +1,36 @@
+import { GiSpellBook } from "react-icons/gi";
 
-const Spells = () => {
 
+export default async function Spell() {
 
+    const res = await fetch("https://hp-api.onrender.com/api/spells");
+    const spells = await res.json();
 
     return (
-        <div id='spells' className='w-full h-screen bg-[#082f49]'>
-            <div className='max-w-[1240px] w-full h-full mx-auto flex justify-center items-center text-center'>
-                <div class='row'>
-                    <p class='text-white'>Spells</p>
+        <div id='staff' className='w-full h-full bg-gradient-to-b from-[#222136] from-10% via-[#272A44] via-30% to-[#2C2D4A] to-90% '>
+            <div class='row'>
+                <div class='col-2' />
+                <div class='col-8'>
+                    <h1 class='flex justify-center font-[kepler] text-4xl font-bold text-white pt-8 mb-10'>Spells</h1>
+                    <div className='max-w-[1240px] max-h-fit w-full h-full mx-auto flex flex-wrap justify-center items-center text-center space-x-5 space-y-5'>
+                        {spells.map((spell) => (
+                            <div key={(spell.id)} class="text-white hover:-translate-y-6">
+                                <GiSpellBook
+                                    class="text-8xl"
+                                    width={250}
+                                    height={200}
+                                />
+                                <ul>
+                                    <li>{spell.name}</li>
+                                </ul>
+                                <ul>
+                                    <li>{spell.description}</li>
+                                </ul>
+                            </div>
+                        ))}
+
+                    </div>
+                    <div class='col-2' />
                 </div>
             </div>
         </div>
@@ -15,4 +38,33 @@ const Spells = () => {
     );
 }
 
-export default Spells;
+// export default Staff;
+
+
+// export default async function Spells() {
+
+//     const res = await fetch("https://hp-api.onrender.com/api/spells");
+//     const spells = await res.json();
+
+//     return (
+//         <div id='spells' className='w-full h-screen bg-gradient-to-b from-[#222136] from-10% via-[#272A44] via-30% to-[#2C2D4A] to-90% '>
+//             <div className='max-w-[1240px] w-full h-full mx-auto flex justify-center items-center text-center'>
+//                 <div class='row'>
+//                     <p class='text-white'>Spells</p>
+//                     {spells.map((spell) => (
+//                         <div key={(spell.id)} class="text-white">
+//                             <ul>
+//                                 <li>{spell.name}</li>
+//                             </ul>
+//                             <ul>
+//                                 <li>{spell.description}</li>
+//                             </ul>
+//                         </div>
+//                     ))}
+//                 </div>
+//             </div>
+//         </div>
+
+//     );
+// }
+
